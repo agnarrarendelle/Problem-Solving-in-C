40605bdd3b045ac65abfb626ea8e599105db74f7
Matt <danny20001102@gmail.com>
2020-10-26 03:51:45 +0800

    C2/test2 improved

--- chapter2/2.c
+++ chapter2/2.c
@@ -3,14 +3,26 @@
 #define G 9.80
 #define offset 0.9
 int main(){
+    char loop = 'y';
+
+    while(loop == 'y'){
 
     double height, cubic, GW;
     printf("What's the height?");
-    scanf("%lf", &height);
+    scanf(" %lf", &height);
     printf("What's the number of cubic?");
-    scanf("%lf", &cubic);
+    scanf(" %lf", &cubic);
 
     GW = LEFT * G * offset * height * (cubic * 1000);
 
-    printf("It's %9.4lf", GW);
+    printf("It's %9.4lf\n", GW);
+
+    printf("Do you want to enter again?(y/n)");
+    scanf(" %c", &loop);
+
+    if(loop != 'y'){
+        loop = 'n';
+        printf("Thanks for your participation\n");
+    }
+    }
 }
