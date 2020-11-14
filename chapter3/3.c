66b56c0919248af9eac2318a078a79952b06f911
Matt <danny20001102@gmail.com>
2020-11-15 01:20:46 +0800

    C3/3.c done

---
+++ chapter3/3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int getNum(int Num);
+double calcuNum(double Num);
+void displayNum(double result);
+
+int main(){
+    int Num;
+    double result;
+    printf("Please enter your number:");
+
+    Num = getNum(Num);
+    result = calcuNum(Num);
+    displayNum(result);
+
+}
+
+int getNum(int Num){
+    scanf(" %d", &Num);
+    return Num;
+}
+
+double calcuNum(double Num){
+    Num = (pow(Num, Num))*(exp(- Num))*(sqrt((2 * Num + 1 / 3) * M_PI));
+}
+
+void displayNum(double result){
+    printf("Value is %.6lf\n", result);
+}
