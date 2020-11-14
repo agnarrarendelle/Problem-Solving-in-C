d9d920396bfaa664a8b3916f6a935dff83be2be6
Matt <danny20001102@gmail.com>
2020-11-15 00:53:15 +0800

    C3/2.c done

---
+++ chapter3/2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void triangle();
+void rectangle();
+
+int main(){
+
+    triangle();
+    rectangle();
+}
+
+void triangle(){
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("****\n");
+}
+
+void rectangle(){
+
+    printf("****\n");
+    printf("****\n");
+    printf("****\n");
+    printf("****\n");
+    
+}
